65ecadc91a2d76db62954219a6a1c025082453cd
David English <david@operaonfilm.com>
2014-09-11 21:37:31 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+KeyframeGen
+========
+
+A tool for creating a series of random keyframes at minimum distances a set clip length
---
+++ app.rb
@@ -0,0 +1,43 @@
+class KeyframeGen
+
+  def initialize(numberOfKeyframesToGenerate, totalFramesOfClip)
+    @numberOfKeyframesToGenerate = numberOfKeyframesToGenerate
+    @totalFramesOfClip = totalFramesOfClip
+    @proximityMinimum = 5
+  end
+
+  def generateKeyframes()
+    arr = []
+    (0..@numberOfKeyframesToGenerate).each do
+      proximityAndUniquenessCondition = false
+      while !proximityAndUniquenessCondition do
+        newFrame = generateFrame()
+        if isUnique(newFrame, arr) && !isTooClose(newFrame, arr)
+          arr.push(newFrame)
+          proximityAndUniquenessCondition = true
+        end
+      end
+    end
+    arr.sort
+  end
+
+  def generateFrame()
+    rand(0..@totalFramesOfClip)
+  end
+
+  def isUnique(newFrame, arr)
+    return true if arr.index(newFrame) == nil
+    false
+  end
+
+  def isTooClose(newFrame, arr)
+    return false if arr.empty?
+    arr.each do |previousFrame|
+      if ((newFrame - previousFrame).abs < @proximityMinimum && (previousFrame - newFrame).abs < @proximityMinimum)
+        return true
+      end
+    end
+    false
+  end
+
+end
